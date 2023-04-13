de0a3bb45d3b5f3079d8caa980b86d17eec1dbeb
evgenyi <fonya93@yandex.ru>
2023-04-13 10:46:55 +0700

    Четные числа от 1 до N

---
+++ Task8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите N: ");
+String userInput = Console.ReadLine() ?? "";
+int N = int.Parse(userInput); ;
+for (int i = 1; i <= N; i++)
+    if (i % 2 == 0)
+        Console.Write($" {i} ");
