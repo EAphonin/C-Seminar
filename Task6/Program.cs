f2a9f9c1f5dd8097d8e4f1aab59ab72f9f46a381
evgenyi <fonya93@yandex.ru>
2023-04-13 10:25:09 +0700

    Проверка четности

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+String userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+if (userNumber % 2 == 0)
+{ Console.WriteLine($"  {userNumber} -> Число чётное  "); }
+else { Console.WriteLine($"  {userNumber} -> Число нечётное  "); }
