be5591b82672d04aa4e26cf2e45c436827b38983
evgenyi <fonya93@yandex.ru>
2023-04-13 09:46:52 +0700

    Поиск наибольшего из 2х чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+String userInput1 = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+String userInput2 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+if (userNumber1 > userNumber2)
+{ Console.WriteLine($" a={userNumber1}, b={userNumber2} -> max={userNumber1}  "); }
+else
+{ Console.WriteLine($" a={userNumber1}, b={userNumber2} -> max={userNumber2}  "); }
